408d8e679d2e39c37a691780df8017326b82f5e9
atiksarker2013 <atiour.islam@gmail.com>
2018-02-22 12:15:01 +0600

    ff

---
+++ AppBootstrapSite1/Controllers/AdministrativeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class AdministrativeController : Controller
+    {
+        // GET: Administrative
+        public ActionResult Chairman()
+        {
+            return View();
+        }
+
+        public ActionResult Principal()
+        {
+            return View();
+        }
+
+        public ActionResult VicePrincipal()
+        {
+            return View();
+        }
+
+        public ActionResult TeacherStudentComitte()
+        {
+            return View();
+        }
+
+        public ActionResult ParentalResponsiblity()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppBootstrapSite1/Models/Administrative.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AppBootstrapSite1.Models
+{
+    public class Administrative
+    {
+        [Display(Name = "Chairman", ResourceType = typeof(Resource))]
+        public string Chairman
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "ChairmanHeading", ResourceType = typeof(Resource))]
+        public string ChairmanHeading
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Principal", ResourceType = typeof(Resource))]
+        public string Principal
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "PrincipalHeading", ResourceType = typeof(Resource))]
+        public string PrincipalHeading
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "VicePrincipal", ResourceType = typeof(Resource))]
+        public string VicePrincipal
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "VicePrincipalHeading", ResourceType = typeof(Resource))]
+        public string VicePrincipalHeading
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "TeacherStudentComitte", ResourceType = typeof(Resource))]
+        public string TeacherStudentComitte
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "TeacherStudentComitteHeading", ResourceType = typeof(Resource))]
+        public string TeacherStudentComitteHeading
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "ParentalResponsiblity", ResourceType = typeof(Resource))]
+        public string ParentalResponsiblity
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "ParentalResponsiblityHeading", ResourceType = typeof(Resource))]
+        public string ParentalResponsiblityHeading
+        {
+            get;
+            set;
+        }
+    }
+}
