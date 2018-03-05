2996901c36c98ba2e9aa982e8c46486438341a51
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 01:23:56 +0600

    fff

---
+++ AppBootstrapSite1/Controllers/AademicInformationController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class AademicInformationController : Controller
+    {
+        // GET: AademicInformation
+        public ActionResult Curriculum()
+        {
+            return View();
+        }
+
+        public ActionResult ClassRoutine()
+        {
+            return View();
+        }
+
+        public ActionResult ExaminationSchedule()
+        {
+            return View();
+        }
+
+        public ActionResult Primary()
+        {
+            return View();
+        }
+
+        public ActionResult Secondary()
+        {
+            return View();
+        }
+
+        public ActionResult HigherSecondary()
+        {
+            return View();
+        }
+
+        public ActionResult HigherEducation()
+        {
+            return View();
+        }
+    }
+}
