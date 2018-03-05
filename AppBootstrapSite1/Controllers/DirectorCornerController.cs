08948cb4f5799dddc01f47fa11ce2a6fa2170bb9
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 02:06:08 +0600

    xx

---
+++ AppBootstrapSite1/Controllers/DirectorCornerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class DirectorCornerController : Controller
+    {
+        // GET: DirectorCorner
+        public ActionResult DirectorIdentity()
+        {
+            return View();
+        }
+
+        public ActionResult DirectorMessage()
+        {
+            return View();
+        }
+    }
+}
