c20257dcea742259a93d3d65588420902636f650
atiksarker2013 <atiour.islam@gmail.com>
2018-03-05 23:30:41 +0600

    rr

---
+++ AppBootstrapSite1/Controllers/OurStoryController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class OurStoryController : Controller
+    {
+        // GET: OurStory
+        public ActionResult HistoryOfInstitution()
+        {
+            return View();
+        }
+
+        public ActionResult AtAGlance()
+        {
+            return View();
+        }
+
+        public ActionResult BordOfGovernor()
+        {
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            return View();
+        }
+
+        public ActionResult ListOfTeacherAndEmployee()
+        {
+            return View();
+        }
+
+        public ActionResult Principal()
+        {
+            return View();
+        }
+
+        public ActionResult VicePrincipal()
+        {
+            return View();
+        }
+
+        public ActionResult TeacherOfHigherEducation()
+        {
+            return View();
+        }
+
+        public ActionResult TeacherOfSecondaryLabel()
+        {
+            return View();
+        }
+
+        public ActionResult TeacherOfPrimaryLabel()
+        {
+            return View();
+        }
+
+        public ActionResult TeacherOfHifzBranch()
+        {
+            return View();
+        }
+
+        public ActionResult Employee()
+        {
+            return View();
+        }
+    }
+}
