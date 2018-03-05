707a58c615c4a984059c4fd833014f768bdf2581
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 02:15:13 +0600

    ddddd

---
+++ AppBootstrapSite1/Controllers/AdmissionProcessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class AdmissionProcessController : Controller
+    {
+        // GET: AdmissionProcess
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
