8fd07aa7375440864192e4c11ce349ec52cd330f
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 17:29:56 +0600

    ff

---
+++ AppBootstrapSite1/Controllers/StudentsIdentityController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class StudentsIdentityController : Controller
+    {
+        // GET: StudentsIdentity
+        public ActionResult PresentAndAbsence()
+        {
+            return View();
+        }
+
+        public ActionResult StudentIdentityAndPassword()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicationForLeaveOfAbsent()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicationForRenuwalIDCard()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicationForResettlement()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicationForRecheckAnswerSheet()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicationForCorrectionNameAndDateOfBirth()
+        {
+            return View();
+        }
+
+        public ActionResult VottingForFoodAndOther()
+        {
+            return View();
+        }
+
+
+
+    }
+}
