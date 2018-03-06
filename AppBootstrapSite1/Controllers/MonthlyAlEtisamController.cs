8e099e88243483538a9ef2e260b51625ec6c1ffb
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 21:16:55 +0600

    rfrrt

---
+++ AppBootstrapSite1/Controllers/MonthlyAlEtisamController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class MonthlyAlEtisamController : Controller
+    {
+        // GET: MonthlyAlEtisam
+        public ActionResult Introduction()
+        {
+            return View();
+        }
+
+        public ActionResult Characteristics()
+        {
+            return View();
+        }
+    }
+}
