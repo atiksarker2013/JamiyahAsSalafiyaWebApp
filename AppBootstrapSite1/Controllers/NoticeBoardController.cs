955b279d38651804e9aed6ea98b281fd0913a406
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 20:25:18 +0600

    ddd

---
+++ AppBootstrapSite1/Controllers/NoticeBoardController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class NoticeBoardController : Controller
+    {
+        // GET: NoticeBoard
+        public ActionResult NoticeForStudent()
+        {
+            return View();
+        }
+
+        public ActionResult AdvertisementForAppointment()
+        {
+            return View();
+        }
+
+        public ActionResult TenderNotice()
+        {
+            return View();
+        }
+    }
+}
