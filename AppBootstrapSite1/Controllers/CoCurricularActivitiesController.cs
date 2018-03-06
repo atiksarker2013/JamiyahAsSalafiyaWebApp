1508629d824fff5e2e019158a675d3de02127970
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 18:53:59 +0600

    ggg

---
+++ AppBootstrapSite1/Controllers/CoCurricularActivitiesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class CoCurricularActivitiesController : Controller
+    {
+        // GET: CoCurricularActivities
+        public ActionResult TrainingForDeliveringSpeech()
+        {
+            return View();
+        }
+
+        public ActionResult QuranStudyAndResearchFourm()
+        {
+            return View();
+        }
+
+
+        public ActionResult HadithStudyAndResearchFourm()
+        {
+            return View();
+        }
+
+        public ActionResult DebatingClub()
+        {
+            return View();
+        }
+
+        public ActionResult IslamicQuizClub()
+        {
+            return View();
+        }
+
+        public ActionResult GeneralQuizClub()
+        {
+            return View();
+        }
+
+        public ActionResult IslamicSongClub()
+        {
+            return View();
+        }
+    }
+}
