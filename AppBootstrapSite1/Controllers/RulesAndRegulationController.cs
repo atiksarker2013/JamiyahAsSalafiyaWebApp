69520c651f337b2c0accbedbf881c7c56c9914e2
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 16:34:10 +0600

    ddd

---
+++ AppBootstrapSite1/Controllers/RulesAndRegulationController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class RulesAndRegulationController : Controller
+    {
+        // GET: RulesAndRegulation
+        public ActionResult Discipline()
+        {
+            return View();
+        }
+
+        public ActionResult ClassAndHoliDaySchedule()
+        {
+            return View();
+        }
+
+        public ActionResult Uniform()
+        {
+            return View();
+        }
+
+        public ActionResult ListOfExam()
+        {
+            return View();
+        }
+
+        public ActionResult MonthlyFees()
+        {
+            return View();
+        }
+
+
+        public ActionResult OtherFees()
+        {
+            return View();
+        }
+
+        public ActionResult Fine()
+        {
+            return View();
+        }
+
+
+    }
+}
