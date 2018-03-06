5459091a30401a0c30a8dae857b08b404794f129
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 20:16:31 +0600

    fff

---
+++ AppBootstrapSite1/Controllers/SpecialEventsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class SpecialEventsController : Controller
+    {
+        // GET: SpecialEvents
+        public ActionResult TrainingAndSeminar()
+        {
+            return View();
+        }
+
+        public ActionResult GamesAndSports()
+        {
+            return View();
+        }
+
+        public ActionResult Picnic()
+        {
+            return View();
+        }
+
+        public ActionResult Farewell()
+        {
+            return View();
+        }
+
+        public ActionResult FestivalReception()
+        {
+            return View();
+        }
+
+        public ActionResult QuranMahfil()
+        {
+            return View();
+        }
+    }
+}
