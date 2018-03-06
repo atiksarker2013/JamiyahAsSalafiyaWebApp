e8d57714388a55f816449b4cd5aa7b6cfe6b225e
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 20:48:49 +0600

    bghh

---
+++ AppBootstrapSite1/Controllers/PrizesAndAchievementsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class PrizesAndAchievementsController : Controller
+    {
+        // GET: PrizesAndAchievements
+        public ActionResult Primary()
+        {
+            return View();
+        }
+
+        public ActionResult JuniourDakhil()
+        {
+            return View();
+        }
+
+        public ActionResult Dakhil()
+        {
+            return View();
+        }
+
+        public ActionResult Alim()
+        {
+            return View();
+        }
+
+        public ActionResult Hounors()
+        {
+            return View();
+        }
+
+        public ActionResult Masters()
+        {
+            return View();
+        }
+    }
+}
