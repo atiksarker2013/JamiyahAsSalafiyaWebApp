55269fc1b30351a19726b732482073de15c13c12
atiksarker2013 <atiour.islam@gmail.com>
2018-03-06 20:35:58 +0600

    rrrr

---
+++ AppBootstrapSite1/Controllers/FacilitiesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBootstrapSite1.Controllers
+{
+    public class FacilitiesController : Controller
+    {
+        // GET: Facilities
+        public ActionResult PrimaryTreatment()
+        {
+            return View();
+        }
+
+        public ActionResult DailyPaperAndMonthlyJournal()
+        {
+            return View();
+        }
+
+        public ActionResult InternetBrowsing()
+        {
+            return View();
+        }
+
+        public ActionResult Library()
+        {
+            return View();
+        }
+    }
+}
